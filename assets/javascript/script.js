de2b29c31485504f96cbe72978dde2ad3f5dc15f
frillic <69489498+crowe828@users.noreply.github.com>
2020-09-17 19:47:06 -0400

    Current day now appears on screen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,10 @@
 // Display current time using Moment.js
+// Log the date to make sure it is correct
 console.log(moment().format("dddd, MMMM Do"));
+// Variable hooked into the current-day element
+var currentDay = document.querySelector("#current-day");
+// Display the current day on the screen
+currentDay.textContent = moment().format("dddd, MMMM Do");
 
 // Click event to save text area content to localStorage
 
